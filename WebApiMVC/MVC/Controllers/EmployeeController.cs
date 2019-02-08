e7fdf0b6bd7c371d57e2e79470abfe2c886c1a94
Roman <46002112+RomanHai94@users.noreply.github.com>
2019-02-09 00:11:09 +0200

    App
    App

---
+++ WebApiMVC/MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+using System.Net.Http;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employees").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<mvcEmployeeModel>>().Result;
+            return View(empList);
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View(new mvcEmployeeModel());
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcEmployeeModel emp)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employees", emp).Result;
+            TempData["SuccessMessage"] = "Save Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApiMVC/WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return db.Employees.ToList();
+        }
+        public Employee GetGetEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            return employee;
+        }
+        [HttpPost]
+        public void CreateFootballer([FromBody] Employee employee)
+        {
+            db.Employees.Add(employee);
+            db.SaveChanges();
+        }
+        [HttpPut]
+        public void EditFootballer(int id, [FromBody] Employee employee)
+        {
+            if (id == employee.EmployeeID)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+        [HttpDelete]
+        public void DeleteFootballer(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee != null)
+            {
+                db.Employees.Remove(employee);
+                db.SaveChanges();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
